71b682f49ff147c73b2139db61e577c8cf6e3fed
shamimdanesh <me@shamimdanesh.com>
2022-09-10 18:37:51 +0430

    add prime number

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-num = int(input("Enter the number: "))
-digits = int(math.log10(num))
-first_digit = int(num / pow(10, digits))
 
-while (num >= 10):
-    num = num // 10
-print('The first digit of number:', first_digit)
 
 
 if __name__ == '__main__':
